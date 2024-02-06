202bca1e7cc491ca4269fe37e8f6f5e193c8d35b
Eric Busboom <eric@civicknowledge.com>
2024-02-06 01:04:59 -0800

    more hacking, almost working

---
+++ docs/src/.vuepress/components/TrinketEmbed.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <iframe
+      :width="computedWidth"
+      :height="computedHeight"
+      frameborder="0"
+      marginwidth="0"
+      marginheight="0"
+      allowfullscreen
+      :src="iframeSrc"
+    ></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TrinketEmbed',
+  props: {
+    type: {
+      type: String,
+      default: 'python',
+    },
+    width: {
+      type: String,
+      default: '100%', // Adjusted to '100%' for responsive design
+    },
+    height: {
+      type: String,
+      default: '500',
+    },
+    code: {
+      type: String,
+      default: 'print("Hello World!")',
+    },
+  },
+  computed: {
+    computedWidth() {
+      return this.width; // Use 100% for full width by default or the value passed in props
+    },
+    computedHeight() {
+      return this.height; // Use default height or the value passed in props
+    },
+    iframeSrc() {
+      // Encode the code and create the URL for the iframe
+      const baseUrl = 'https://trinket.io/tools/1.0/embed/';
+      const encodedCode = encodeURIComponent(this.code.trim());
+      return `${baseUrl}${this.type}#code=${encodedCode}`;
+    },
+  },
+  mounted() {
+    // Additional JavaScript for iframe manipulation can go here
+  },
+};
+</script>
+
+<style scoped>
+/* Your component-specific styles here */
+</style>
---
+++ docs/src/index.md
@@ -0,0 +1,18 @@
+---
+home: true
+heroImage:  /images/word-logo.png
+tagline: Introduction to Python
+actionText: Get Started!
+actionLink: /lessons
+#features:
+#- title: Feature 1 Title
+#  details: Feature 1 Description
+#- title: Feature 2 Title
+#  details: Feature 2 Description
+#- title: Feature 3 Title
+#  details: Feature 3 Description
+#footer: Made by Eric Busboom  with ❤️
+---
+
+
+Here is come content on the page
